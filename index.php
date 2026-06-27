117a2078146ce3d1270fe625f7c07f495225e7f5
alieffaisaladriansyah <alieffaisal6@gmail.com>
2026-06-27 23:27:07 +0700

    update

--- index.php
+++ index.php
@@ -65,6 +65,7 @@ if (isset($_SESSION['id_pengguna']) && ($_SESSION['peran'] ?? '') !== 'admin') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kel1 | reservasi_hotel</title>
+    <link rel="icon" type="image/png" href="assets/logo/favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
